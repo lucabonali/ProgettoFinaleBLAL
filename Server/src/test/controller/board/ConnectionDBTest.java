36d0794c5c4b4337d566697ff99832f76e293bac
Andrea <lampa9559@gmail.com>
2017-05-17 14:06:54 +0200

    41^ aggiornamento: aggiunto qualche stupido test

---
+++ Server/src/test/controller/board/ConnectionDBTest.java
@@ -0,0 +1,27 @@
+package test.controller.board;
+
+import main.controller.board.ConnectionDB;
+import org.junit.jupiter.api.Test;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * @author lampa
+ */
+class ConnectionDBTest {
+
+    @Test
+    void executeQuery() throws SQLException {
+        ConnectionDB connectionDB = new ConnectionDB();
+        ResultSet rs = connectionDB.executeQuery("SELECT * FROM cards");
+        assertTrue(rs != null);
+    }
+
+    @Test
+    void closeConnection() {
+    }
+
+}
---
+++ Server/src/test/controller/effects/FixedIncrementEffectTest.java
@@ -0,0 +1,16 @@
+package test.controller.effects;
+
+/**
+ * @author Luca
+ * @author Andrea
+ */
+class FixedIncrementEffectTest {
+    @org.junit.jupiter.api.Test
+    void active() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void createInstance() {
+    }
+
+}
