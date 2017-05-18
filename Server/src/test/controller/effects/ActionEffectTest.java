cc9bd4d5fde32cc5a1661571a98a6d5eeeb4486b
Andrea <lampa9559@gmail.com>
2017-05-18 23:43:49 +0200

    50^ aggiornamento: aggiunto ActionEffctTest

---
+++ Server/src/test/controller/effects/ActionEffectTest.java
@@ -0,0 +1,24 @@
+package test.controller.effects;
+
+import main.api.exceptions.NewActionException;
+import main.controller.effects.ActionEffect;
+import main.servergame.rmi.PlayerRMI;
+import org.junit.Test;
+
+/**
+ * @author Luca
+ * @author Andrea
+ */
+public class ActionEffectTest {
+    @Test(expected = NewActionException.class)
+    public void active() throws Exception {
+        PlayerRMI p = new PlayerRMI("luca");
+        ActionEffect ae = new ActionEffect('a', 4);
+        ae.active(p);
+    }
+
+    @Test
+    public void createInstance() throws Exception {
+    }
+
+}
--- Server/src/test/controller/effects/ActionValueIncrementEffectTest.java
+++ Server/src/test/controller/effects/ActionValueIncrementEffectTest.java
@@ -3,9 +3,12 @@ package test.controller.effects;
 import main.api.types.FamilyMemberType;
 import main.controller.actionSpaces.Action;
 import main.controller.actionSpaces.singleActionSpaces.HarvestActionSpace;
+import main.controller.actionSpaces.singleActionSpaces.ProductionActionSpace;
 import main.controller.effects.ActionValueIncrementEffect;
 import main.servergame.rmi.PlayerRMI;
 import org.junit.Test;
+
+import java.rmi.RemoteException;
 
 import static org.junit.Assert.assertEquals;
 
@@ -16,7 +19,18 @@ import static org.junit.Assert.assertEquals;
  */
 public class ActionValueIncrementEffectTest {
     @Test
-    public void active() throws Exception {
+    public void active1() throws Exception {
+        PlayerRMI p = new PlayerRMI("andrea");
+        p.createPersonalBoard(1);
+        HarvestActionSpace has = new HarvestActionSpace(1);
+        p.getPersonalBoard().setCurrentAction(new Action(has, 3, p.getFamilyMember(FamilyMemberType.ORANGE_DICE), p));
+        ActionValueIncrementEffect avie = new ActionValueIncrementEffect(new ProductionActionSpace(1), 2);
+        avie.active(p);
+        assertEquals(3, p.getPersonalBoard().getCurrentAction().getValue());
+    }
+
+    @Test
+    public void active2() throws RemoteException {
         PlayerRMI p = new PlayerRMI("andrea");
         p.createPersonalBoard(1);
         HarvestActionSpace has = new HarvestActionSpace(1);
