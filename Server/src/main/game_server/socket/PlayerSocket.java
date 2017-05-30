cb863e3c73ae6d56f8cd3e0e53819dc059ebaccc
Andrea <lampa9559@gmail.com>
2017-05-30 22:21:44 +0200

    118^ aggiornamento: penso di aver terminato anche la parte socket, non ancora controllate.

--- Server/src/main/game_server/socket/PlayerSocket.java
+++ Server/src/main/game_server/socket/PlayerSocket.java
@@ -1,6 +1,7 @@
 package main.game_server.socket;
 
 import main.api.messages.MessageAction;
+import main.api.messages.MessageNewAction;
 import main.api.messages.SocketProtocol;
 import main.api.types.CardType;
 import main.api.types.ResourceType;
@@ -37,32 +38,83 @@ public class PlayerSocket extends AbstractPlayer implements Runnable {
 
     @Override
     public void gameIsStarted(List<Integer> idList, List<String> codeExcomList) throws RemoteException {
-
+        try {
+            out.writeObject(SocketProtocol.IS_GAME_STARTED);
+            out.flush();
+            out.writeInt(getIdPlayer());
+            out.flush();
+            out.writeObject(idList);
+            out.flush();
+            out.writeObject(codeExcomList);
+            out.flush();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void isYourTurn() throws RemoteException {
-        //mandare un messaggio particolare per notificare che è il suo turno
+        try {
+            out.writeObject(SocketProtocol.YOUR_TURN);
+            out.flush();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void isYourExcommunicationTurn() throws RemoteException {
-        //da implementare
+        try {
+            out.writeObject(SocketProtocol.YOUR_EXCOMMUNICATION_TURN);
+            out.flush();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void youWin() throws RemoteException {
-        printMsgToClient("Hai vinto, complimenti!!");
+        try {
+            out.writeObject(SocketProtocol.GAME_ENDED);
+            out.flush();
+            out.writeObject("Hai vinto, complimenti!!");
+            out.flush();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     @Override
     public void youLose() throws RemoteException {
-        printMsgToClient("Hai perso :(");
+        try {
+            out.writeObject(SocketProtocol.GAME_ENDED);
+            out.flush();
+            out.writeObject("Hai perso :(");
+            out.flush();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void notifyNewAction(int value, char codeAction) throws RemoteException {
-
+        try {
+            out.writeObject(SocketProtocol.NEW_ACTION);
+            out.flush();
+            out.writeInt(value);
+            out.flush();
+            out.writeChar(codeAction);
+            out.flush();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -72,47 +124,120 @@ public class PlayerSocket extends AbstractPlayer implements Runnable {
 
     @Override
     public void updateMove() throws RemoteException {
-        //devo mandare al giocatore tutte le sue risorse e tutte le sue carte
+        try {
+            out.writeObject(SocketProtocol.UPDATE_RESOURCES);
+            out.flush();
+            out.writeObject(getPersonalBoard().getQtaResources());
+            out.flush();
+            out.writeObject(SocketProtocol.UPDATE_PERSONAL_CARDS);
+            out.flush();
+            out.writeObject(getPersonalBoard().getPersonalCardsMap());
+            out.flush();
+            getGame().notifyAllPlayers(this, getIdPlayer(), getPersonalBoard().getPersonalCardsMap(), getPersonalBoard().getQtaResources());
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void updateOpponentMove(int id, Map<CardType, List<String>> personalcardsMap, Map<ResourceType, Integer> qtaResourcesMap) throws RemoteException {
-        //da implementare
+        try {
+            out.writeObject(SocketProtocol.OPPONENT_MOVE);
+            out.flush();
+            out.writeInt(id);
+            out.flush();
+            out.writeObject(personalcardsMap);
+            out.flush();
+            out.writeObject(qtaResourcesMap);
+            out.flush();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void notifyRollDice() throws RemoteException {
-        //da implementare
+        try {
+            out.writeObject(SocketProtocol.HAVE_TO_SHOT);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void excommunicate(int id, int period) throws RemoteException {
-        //da implementare
+        getGame().notifyAllPlayers(this, period);
+        opponentExcommunicate(id, period);
     }
 
     @Override
     public void opponentExcommunicate(int idPlayer, int period) throws RemoteException {
-        // da implementare
+        try {
+            out.writeObject(SocketProtocol.EXCOMMUNICATE);
+            out.flush();
+            out.writeInt(idPlayer);
+            out.flush();
+            out.writeInt(period);
+            out.flush();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void sendDicesValues(int orange, int white, int black) throws RemoteException {
-        //da implementare
+        try {
+            out.writeObject(SocketProtocol.DICE_VALUES);
+            out.flush();
+            out.writeInt(orange);
+            out.flush();
+            out.writeInt(white);
+            out.flush();
+            out.writeInt(black);
+            out.flush();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void initializeBoard(List<DevelopmentCard> towersCardsList) throws RemoteException {
-        //da implementare
+        try {
+            out.writeObject(SocketProtocol.TOWERS_CARDS);
+            out.flush();
+            out.writeObject(towersCardsList);
+            out.flush();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void notifyEndMove() throws RemoteException {
-        //da implementare
+        try {
+            out.writeObject(SocketProtocol.END_MOVE);
+            out.flush();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void notifyPrivilege() throws RemoteException {
-        //da implementare
+        try {
+            out.writeObject(SocketProtocol.PRIVILEGE);
+            out.flush();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private void printMsgToClient(String content){
@@ -147,6 +272,10 @@ public class PlayerSocket extends AbstractPlayer implements Runnable {
                         doAction((MessageAction) msg);
                         break;
                     }
+                    else if (msg instanceof MessageNewAction) {
+                        doNewAction((MessageNewAction) msg);
+                        break;
+                    }
                     else if (msg instanceof SocketProtocol){
                         switch ((SocketProtocol) msg){
                             case SHOT_DICE:
@@ -158,6 +287,17 @@ public class PlayerSocket extends AbstractPlayer implements Runnable {
                             case EXCOMMUNICATION_CHOICE:
                                 boolean choice = in.readBoolean();
                                 excommunicationChoice(choice);
+                                break;
+                            case END_MOVE:
+                                endMove();
+                                break;
+                            case SURRENDER:
+                                surrender();
+                                break;
+                            case CONVERT_PRIVILEGE:
+                                int qta = in.readInt();
+                                ResourceType type = (ResourceType) in.readObject();
+                                convertPrivilege(qta, type);
                                 break;
                         }
                     }
